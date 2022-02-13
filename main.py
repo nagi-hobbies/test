2f74a4c01812e544f4e3a841e1f2d785f96ab0ca
nagi-navi <naginagi.hoby@gmail.com>
2022-02-13 23:17:58 +0900

    1st

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import sympy as sp
+
+from latex2sympy.latex2sympy import process_latex
+
+st.title('Calculator')
+
+'''
+## target formula
+'''
+
+tgt = st.text_input('')
+
+st.latex(tgt)
+
+x = sp.Symbol('x')
+
+f = process_latex.process_sympy(tgt)
+
+st.latex(sp.solve(f, x)[0])
